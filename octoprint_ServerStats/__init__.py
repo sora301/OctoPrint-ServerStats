f2ea5ab69f09fc8dca446b221fd650bef026effc
Jeff Wight <JeffyW@gmail.com>
2017-06-16 06:58:40 -0700

    Initial implementation and Temperature

--- octoprint_ServerStats/__init__.py
+++ octoprint_ServerStats/__init__.py
@@ -1,72 +1,137 @@
 # coding=utf-8
 from __future__ import absolute_import
 
-### (Don't forget to remove me)
-# This is a basic skeleton for your plugin's __init__.py. You probably want to adjust the class name of your plugin
-# as well as the plugin mixins it's subclassing from. This is really just a basic skeleton to get you started,
-# defining your plugin as a template plugin, settings and asset plugin. Feel free to add or remove mixins
-# as necessary.
-#
-# Take a look at the documentation on what other plugin mixins are available.
-
 import octoprint.plugin
+from octoprint.util import RepeatedTimer
+import sys
+import re
 
-class ServerstatsPlugin(octoprint.plugin.SettingsPlugin,
-                        octoprint.plugin.AssetPlugin,
-                        octoprint.plugin.TemplatePlugin):
+class ServerStatsPlugin(octoprint.plugin.StartupPlugin,
+                        octoprint.plugin.SettingsPlugin):
 
-	##~~ SettingsPlugin mixin
+    def __init__(self):
+        self._timer = None
+        
+        self.debugMode = False
+        self.hardware = None
+        
+        self.tempFunc = None
 
-	def get_settings_defaults(self):
-		return dict(
-			# put your plugin's default settings here
-		)
+    ##~~ SettingsPlugin mixin
+    def get_settings_defaults(self):
+        return dict(
+            # put your plugin's default settings here
+        )
 
-	##~~ AssetPlugin mixin
+    ##~~ Softwareupdate hook
+    def get_update_information(self):
+        # Define the configuration for your plugin to use with the Software Update
+        # Plugin here. See https://github.com/foosel/OctoPrint/wiki/Plugin:-Software-Update
+        # for details.
+        return dict(
+            ServerStats=dict(
+                displayName="ServerStats",
+                displayVersion=self._plugin_version,
 
-	def get_assets(self):
-		# Define your plugin's asset files to automatically include in the
-		# core UI here.
-		return dict(
-			js=["js/ServerStats.js"],
-			css=["css/ServerStats.css"],
-			less=["less/ServerStats.less"]
-		)
+                # version check: github repository
+                type="github_release",
+                user="JeffyW",
+                repo="OctoPrint-ServerStats",
+                current=self._plugin_version,
 
-	##~~ Softwareupdate hook
+                # update method: pip
+                pip="https://github.com/JeffyW/OctoPrint-ServerStats/archive/{target_version}.zip"
+            )
+        )
 
-	def get_update_information(self):
-		# Define the configuration for your plugin to use with the Software Update
-		# Plugin here. See https://github.com/foosel/OctoPrint/wiki/Plugin:-Software-Update
-		# for details.
-		return dict(
-			ServerStats=dict(
-				displayName="Serverstats Plugin",
-				displayVersion=self._plugin_version,
+    ##~~ StartupPlugin mixin
+    def on_after_startup(self):
+        self._logger.info("Starting SystemStats")
+        if self.debugMode:
+            self.hardware = "Debug"
+        elif sys.platform.startswith("linux2"):
+            with open("/proc/cpuinfo", "r") as infile:
+                cpuinfo = infile.read()
 
-				# version check: github repository
-				type="github_release",
-				user="JeffyW",
-				repo="OctoPrint-ServerStats",
-				current=self._plugin_version,
+            # Match a line like "Hardware   : BCM2709"
+            match = re.search("^Hardware\s+:\s+(\w+)$", cpuinfo, flags=re.MULTILINE | re.IGNORECASE)
 
-				# update method: pip
-				pip="https://github.com/JeffyW/OctoPrint-ServerStats/archive/{target_version}.zip"
-			)
-		)
+            if match is not None:
+                self.hardware = match.group(1)
+                self._logger.debug("Hardware: %s", self.hardware)
 
+            import os.path
+            if os.path.isfile("/sys/devices/virtual/thermal/thermal_zone0/temp"):
+                self.tempFunc = self.temp_from_thermal
+
+                self.hardware_overrides()
+            self.start_timer(5.0)
+
+    def start_timer(self, interval):
+        self._logger.debug("Starting RepeatedTimer with interval: %d" % interval)
+        self._timer = RepeatedTimer(interval, self.get_system_stats, run_first=True)
+        self._timer.start()
+
+    def get_system_stats(self):
+        self._logger.info("Collecting system stats.")
+
+        if self.debugMode:
+            temp = randrange_float(5, 60, 0.1)
+        elif self.tempFunc is not None:
+            temp = self.tempFunc()
+
+        self._plugin_manager.send_plugin_message(self._identifier, dict(temp=temp))
+
+    def hardware_overrides(self):
+        if self.hardware == "BCM2708":
+            self._logger.debug("Pi 1")
+            self.tempFunc = self.temp_from_vcgencmd
+        elif self.hardware == "BCM2709":
+            self._logger.debug("Pi 2")
+            self.tempFunc = self.temp_from_vcgencmd
+        elif self.hardware == "sun50iw1p1":
+            self._logger.debug("Pine A64")
+    
+    def temp_from_thermal(self):
+        self._logger.debug("Reading: /sys/devices/virtual/thermal/thermal_zone0/temp")
+        with open("/sys/devices/virtual/thermal/thermal_zone0/temp", "r") as content_file:
+            p = content_file.read().strip()
+        self._logger.debug("Temperature: %s" % p)
+        return p
+
+    def temp_from_vcgencmd(self):
+        self._logger.debug("Running: /opt/vc/bin/vcgencmd measure_temp")
+        temp = None
+        from sarge import run, Capture
+        p = run("/opt/vc/bin/vcgencmd measure_temp", stdout=Capture())
+        if p.returncode==1:
+            self._logger.error("Command failed.")
+        else:
+            p = p.stdout.text
+            self._logger.debug("Command output: %s" % p)
+            match = re.search("=(.*)\'", p)
+            if not match:
+                self._logger.error("Invalid temperature format.")
+            else:
+                temp = match.group(1)
+                self._logger.debug("Temperature: %s" % temp)
+        return temp
+
+    def randrange_float(start, stop, step):
+        import random
+        return random.randint(0, int((stop - start) / step)) * step + start
 
 # If you want your plugin to be registered within OctoPrint under a different name than what you defined in setup.py
 # ("OctoPrint-PluginSkeleton"), you may define that here. Same goes for the other metadata derived from setup.py that
 # can be overwritten via __plugin_xyz__ control properties. See the documentation for that.
-__plugin_name__ = "Serverstats Plugin"
+__plugin_name__ = "ServerStats Plugin"
 
 def __plugin_load__():
-	global __plugin_implementation__
-	__plugin_implementation__ = ServerstatsPlugin()
+    global __plugin_implementation__
+    __plugin_implementation__ = ServerStatsPlugin()
 
-	global __plugin_hooks__
-	__plugin_hooks__ = {
-		"octoprint.plugin.softwareupdate.check_config": __plugin_implementation__.get_update_information
-	}
+    global __plugin_hooks__
+    __plugin_hooks__ = {
+        "octoprint.plugin.softwareupdate.check_config": __plugin_implementation__.get_update_information
+    }
 
